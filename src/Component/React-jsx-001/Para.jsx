26e2ab8731317610bb00e7cf57da7efa2e5e7842
Ravi Yadav <raviyadav29032003@gmail.com>
2023-08-04 21:51:04 +0530

    commit

---
+++ src/Component/React-jsx-001/Para.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Para() {
+  return (
+    <div style={{width:"100%",  boxShadow:"0 0 10px black",padding:"10px",backgroundColor:"red",color:"white"}}>
+        <h1>About US</h1>
+        <h3>Inserting a Large Block of HTML</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eligendi totam ratione nemo quisquam atque, nisi ipsum ducimus quaerat ipsa tempore esse porro velit nobis, voluptates, magnam ea inventore suscipit cum. Exercitationem, at iusto tempora officiis amet explicabo mollitia. Inventore quaerat quisquam est quidem deleniti enim sunt ad, hic vitae officiis, similique ea et labore corrupti quod libero? Est culpa placeat assumenda ducimus mollitia voluptatibus modi odit veritatis, ipsam necessitatibus aperiam inventore dolore minus facilis dolores laborum porro ratione temporibus natus earum, quidem tempora, eveniet numquam impedit. Nobis, aspernatur! Ullam voluptatem odio architecto sequi repellendus iusto ipsam, molestias fuga! Doloremque neque, autem libero aliquid atque distinctio numquam amet beatae, ratione vel quas? Nisi, quibusdam ipsum nam modi aspernatur eum ut ex optio temporibus quod magnam adipisci architecto earum officia ad amet error accusantium, pariatur itaque unde animi facere. Eum in magni quis quasi dignissimos tenetur nostrum. Debitis tempore impedit voluptatem porro vel maiores nisi autem veritatis dolore saepe rem necessitatibus vero nostrum quaerat a, minima dolor, explicabo modi voluptatibus repellat deserunt dolorem soluta! Quaerat, dolorem est praesentium aliquid iure molestiae consequuntur! Ducimus obcaecati quo natus maiores qui ratione autem quia eaque. Vel voluptatibus reiciendis ea voluptate repellat laudantium exercitationem vero.</p>
+    </div>
+  )
+}
+
+export default Para
---
+++ src/Component/React-jsx-002/Para01.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Para01() {
+  return (
+    <div>
+        <h3>The Evaluation of Airbnb's Frontend</h3>
+    </div>
+  )
+}
+
+export default Para01;
