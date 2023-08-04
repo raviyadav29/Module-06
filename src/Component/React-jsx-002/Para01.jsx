2892ce6a8d98c971cf3b54be190811a577db7ec4
Ravi Yadav <raviyadav29032003@gmail.com>
2023-08-05 00:02:11 +0530

    commit

--- src/Component/React-jsx-002/Para01.jsx
+++ src/Component/React-jsx-002/Para01.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Para01() {
   return (
-    <div>
-        <h3>The Evaluation of Airbnb's Frontend</h3>
+    <div style={{backgroundColor:"red",width:"20%",textAlign:"center",marginLeft:"40%",color:"white",padding:"10px",marginTop:"10px"}}>
+        <h2>The Evaluation of Airbnb's Frontend</h2>
+        <p>@spikebrehm</p>
+        <img width={100} height={60} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDndK9h9MitzRALwNAjM296T0niHfibpFyoA&usqp=CAU'/>
     </div>
   )
 }
